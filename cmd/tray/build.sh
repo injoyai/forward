241c70a94535ef6d9b0acad450345e301acc2d28
injoyai <1113655791@qq.com>
2026-01-21 12:07:26 +0800

    优化

--- cmd/tray/build.sh
+++ cmd/tray/build.sh
@@ -1,8 +1,9 @@
-name="forward-win-amd64"
+name="forward"
 GOOS=windows GOARCH=amd64 go build -v -ldflags="-H windowsgui -w -s" -o ./$name.exe
 echo "$name 编译完成..."
 upx -9 -k "./$name.exe"
 rm "./$name.ex~"
 rm "./$name.000"
 echo "压缩完成..."
+cmd.exe /c "i upload minio forward.exe"
 sleep 2
--- cmd/tray/main.go
+++ cmd/tray/main.go
@@ -6,20 +6,21 @@ import (
 	"github.com/injoyai/forward"
 	"github.com/injoyai/goutil/oss"
 	"github.com/injoyai/goutil/oss/tray"
+	"github.com/injoyai/logs"
 	"github.com/injoyai/lorca"
 )
 
 func main() {
-	port := 60073
+	port := 40073
 	db := oss.UserInjoyDir("/forward/database/forward.db")
-	go forward.Run(port, db)
+	go func() { logs.PanicErr(forward.Run(port, db)) }()
 	tray.Run(
 		tray.WithIco(ico),
 		tray.WithHint("端口转发"),
 		func(s *tray.Tray) {
 			s.AddMenu().SetName("配置").SetIcon(tray.IconSetting).OnClick(func(m *tray.Menu) {
 				lorca.Run(&lorca.Config{
-					Width:  800,
+					Width:  920,
 					Height: 600,
 					Index:  fmt.Sprintf("http://localhost:%d", port),
 				})
